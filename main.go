444b2caf6b5c840cab2636a07c378d0acdb5b36c
Lucas Pirola <lucaspirola@gmail.com>
2017-02-26 18:53:17 -0300

    Add download and parse list mobilizations

--- main.go
+++ main.go
@@ -3,19 +3,46 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"encoding/json"
+	"time"
 	"os"
 )
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+    r, err := myClient.Get(url)
+    if err != nil {
+        return err
+    }
+    defer r.Body.Close()
+
+    return json.NewDecoder(r.Body).Decode(target)
+}
+
+type Mobilization struct {
+    ID int
+		Name string
+		Slug string
+		CustomDomain string
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	if port == "" {
 		e.Logger.Fatal("$PORT must be set")
 	}
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		mob := new(Mobilization) // or &Foo{}
+		getJson("https://api-ssl.reboo.org/mobilizations", mob)
+		return c.String(http.StatusOK, mob.Name)
 	})
 
 	e.Logger.Fatal(e.Start(":" + port))
