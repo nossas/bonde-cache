94a5c3f39598c02c1fd142de09f04284789bcd93
Lucas Pirola <lucaspirola@gmail.com>
2017-06-22 22:11:28 -0300

    Customize tls cipher accepted

--- main.go
+++ main.go
@@ -271,24 +271,24 @@ func main() {
 					tls.X25519,
 				},
 				PreferServerCipherSuites: true,
-				// CipherSuites: []uint16{
-				// 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				// 	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				// 	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				// 	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-				// or
-				//  tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				//  tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				//  tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-				//  tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-				//  tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				//  tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				CipherSuites: []uint16{
+					// 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					// 	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					// 	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					// 	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					// or
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 
-				// Best disabled, as they don't provide Forward Secrecy,
-				// but might be necessary for some clients
-				//  tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				//  tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-				// },
+					// Best disabled, as they don't provide Forward Secrecy,
+					// but might be necessary for some clients
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				},
 			}
 			s.TLSConfig = cfg
 			s.TLSConfig.GetCertificate = e.AutoTLSManager.GetCertificate
